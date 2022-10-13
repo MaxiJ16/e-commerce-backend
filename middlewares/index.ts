67c97e14694b31751574a093e2ec988b826734d9
Maxi Jofré <maxijofre.c@gmail.com>
2022-10-12 21:19:39 -0300

    change cors

--- middlewares/index.ts
+++ middlewares/index.ts
@@ -1,34 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import parseBearerToken from "parse-bearer-token";
 import { decode } from "lib/jwt";
-import type { NextRequest } from "next/server";
-
-export function middlewareCors(req: NextRequest) {
-  return new Response("", {
-    status: 204,
-    headers: {
-      "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-      "Access-Control-Allow-Headers": req.headers.get(
-        "Access-Control-Request-Headers"
-      ),
-      Vary: "Access-Control-Request-Headers",
-      "Content-Length": "0",
-    },
-  });
-}
 
 export function authMiddleware(callback) {
-  return function (
-    req: NextApiRequest,
-    res: NextApiResponse,
-    request: NextRequest
-  ) {
-    if (request.method == "OPTIONS") {
-      middlewareCors(request);
-    }
-    
+  return function (req: NextApiRequest, res: NextApiResponse) {
     const token = parseBearerToken(req);
 
     if (!token) {
